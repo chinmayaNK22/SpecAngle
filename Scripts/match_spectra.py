fc02cbd95336d118df22d7183d57c523ab42c3c6
Chinmaya Narayana <58813915+chinmayaNK22@users.noreply.github.com>
2023-08-21 19:10:04 +0530

    Update match_spectra.py

--- Scripts/match_spectra.py
+++ Scripts/match_spectra.py
@@ -15,8 +15,8 @@ def peak_match(ex_frag_mz, theo_mz, ex_frag_intensity, theo_frag_intensity, ion_
 
     matching_values = np.array([], dtype=float)
     matching_ions = np.array([], dtype='object')
-    matching_exp_intense = np.array([], dtype='f')
-    matching_pred_intense = np.array([], dtype='f')
+    matching_exp_intense = np.array([], dtype='d')
+    matching_pred_intense = np.array([], dtype='d')
     
     # Loop through the values in thoeretical mz array and identify the matching experimental mz values within given fragment tolerance
     for index, value in np.ndenumerate(theo_mz):
@@ -33,23 +33,13 @@ def peak_match(ex_frag_mz, theo_mz, ex_frag_intensity, theo_frag_intensity, ion_
                     matching_ions = np.append(matching_ions, ion_type[index])
                     matching_exp_intense = np.append(matching_exp_intense, ex_frag_intensity[close_exp_idx])
                     matching_pred_intense = np.append(matching_pred_intense, theo_frag_intensity[index])
-                    #if close_exp_idx == len(ex_frag_intensity)-1:
-                    #    matching_intense = np.append(matching_intense, ex_frag_intensity[-1])
-                    #
-                    #else:
-                    #    matching_intense = np.append(matching_intense, ex_frag_intensity[close_exp_idx])
+
                         
             else:
                 exp_mz_idx = np.where(np.abs(ex_frag_mz - value) <= float(mz_range(tolerance, value, unit)))[0]
                 if len(np.where(matching_values == exp_mz)[0]) == 0: ### Exclude the mz values which already present the the final numpy array
                     matching_values = np.append(matching_values, exp_mz)
                     matching_ions = np.append(matching_ions, ion_type[index])
-                    #print (exp_mz_idx, exp_mz, value, ion_type[index], len(ex_frag_intensity), len(ex_frag_mz), ex_frag_mz, ex_frag_intensity[exp_mz_idx], ex_frag_intensity)
-
-                    #if exp_mz_idx == len(ex_frag_intensity)-1 or exp_mz_idx == len(ex_frag_intensity):
-                    #    matching_intense = np.append(matching_intense, ex_frag_intensity[-1])
-                    #else:
-                    
                     matching_exp_intense = np.append(matching_exp_intense, ex_frag_intensity[exp_mz_idx])
                     matching_pred_intense = np.append(matching_pred_intense, theo_frag_intensity[index])
                 
